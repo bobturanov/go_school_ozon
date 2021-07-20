22bb2f41dd35a355d5d5a8b164117f8531454651
galileo <galileo@fixmost.com>
2021-07-21 02:35:30 +0300

    docstring + tests + .gitignore

--- solution.py
+++ solution.py
@@ -3,9 +3,24 @@ from typing import Tuple, List
 
 
 def find_subarray_with_max_sum(nums: List[Number]) -> Tuple[int, int]:
-    # TODO docstring
+    """
+    Returns start and end indexes of subarray with max sum.
+
+            Parameters:
+                    nums (list): list of numbers
+
+            Returns:
+                    (start, end) (tuple):  start and end indexes of subarray
+    """
+
+    if not nums:
+        raise ValueError('Empty list, try not empty list')
+
+    if not isinstance(nums, list):
+        raise TypeError(f'Received {type(nums)}, try list')
+
     current_max_sum = 0
-    max_sum = float('-inf')  # TODO подумать над 1ым эл.
+    max_sum = float('-inf')
     start = 0
     end = 0
     new_start = 0
@@ -23,6 +38,3 @@ def find_subarray_with_max_sum(nums: List[Number]) -> Tuple[int, int]:
             new_start = i + 1
 
     return start, end
-
-
-print(find_subarray_with_max_sum([-15, 20, 40, -10, 15]))  # TODO написать тесты
